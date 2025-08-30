97799b70026cb881472f0869f6be895b2a38ae81
Tulqin_Muxtorov98 <104984928+Muxtorov98@users.noreply.github.com>
2025-08-30 11:48:38 +0500

    redis image

--- src/config/bull.config.ts
+++ src/config/bull.config.ts
@@ -2,7 +2,7 @@ import { BullModuleOptions } from '@nestjs/bull';
 
 export const BullConfig: BullModuleOptions = {
   redis: {
-    host: process.env.REDIS_HOST || 'redis',
+    host: process.env.REDIS_HOST || '127.0.0.1',
     port: Number(process.env.REDIS_PORT) || 6379,
     password: process.env.REDIS_PASSWORD || undefined,
   },
